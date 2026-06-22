56c02c0b02271c28999e4179f7c91dcedf7a22f2
Tiago Souza <tirigopeixe@gmail.com>
2026-06-21 21:28:49 -0300

    implement main

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-from services import supabase as supa_service
-from services import whatsapp as whats_service
+from services import supabase as supa_service;
+from services import whatsapp as whats_service;
 
-contacts = supa_service.getContacts()
+try:
+    response = supa_service.getContacts();
 
-res = whats_service.sendMessage(contacts["data"][1]["number"], contacts["data"][1]["name"])
+    if(response["type"] == "Error"):
+        raise Exception(response["message"]);
+
+    contacts = response["data"];
+
+    for contact in contacts:
+        name = contact["name"];
+        number = contact["number"];
+
+        res = whats_service.sendMessage(number, name);
+
+        if(res["type"] == "Error"):
+            print("Falha ao enviar mensagem para numero: ", number);
+        else:
+            print("Mensagem enviada com sucesso para o numero: ", number);
+    
+except Exception as e:
+    print("ERROR: ", e);
